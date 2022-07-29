ce50cc3787a0f4845d8e7ae0c2a63b6400d47f19
Arik Kfir <arik@kfirs.com>
2022-07-29 18:55:57 +0300

    WIP

--- pkg/resource.go
+++ pkg/resource.go
@@ -11,27 +11,33 @@ type RNode struct {
 }
 
 func (r *RNode) GetAPIVersion() (string, error) {
-	if value, err := GetMappingKeyValue(r.N, "apiVersion"); err != nil {
+	if value, err := GetMappingKeyValue(r.N, "apiVersion", yaml.ScalarNode, "!!str"); err != nil {
 		return "", err
+	} else if value == nil {
+		return "", nil
 	} else {
 		return value.(string), nil
 	}
 }
 
 func (r *RNode) GetKind() (string, error) {
-	if value, err := GetMappingKeyValue(r.N, "kind"); err != nil {
+	if value, err := GetMappingKeyValue(r.N, "kind", yaml.ScalarNode, "!!str"); err != nil {
 		return "", err
+	} else if value == nil {
+		return "", nil
 	} else {
 		return value.(string), nil
 	}
 }
 
 func (r *RNode) GetAPIGroupAndVersion() (string, string, error) {
-	if apiVersion, err := r.GetAPIVersion(); err != nil {
+	if value, err := r.GetAPIVersion(); err != nil {
 		return "", "", fmt.Errorf("failed getting apiVersion: %w", err)
-	} else if lastSlashIndex := strings.LastIndex(apiVersion, "/"); lastSlashIndex < 0 {
-		return "", apiVersion, nil
+	} else if value == "" {
+		return "", "", fmt.Errorf("apiVersion is missing")
+	} else if lastSlashIndex := strings.LastIndex(value, "/"); lastSlashIndex < 0 {
+		return "", value, nil
 	} else {
-		return apiVersion[0:lastSlashIndex], apiVersion[lastSlashIndex+1:], nil
+		return value[0:lastSlashIndex], value[lastSlashIndex+1:], nil
 	}
 }
---
+++ pkg/resource_metadata_test.go
@@ -0,0 +1,153 @@
+package pkg
+
+import (
+	"gopkg.in/yaml.v3"
+	"testing"
+)
+
+func TestRNodeGetNamespace(t *testing.T) {
+	n1 := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("metadata: {namespace: ns, name: r}"), n1); err != nil {
+		t.Fatal(err)
+	}
+	r1 := &RNode{N: n1}
+	if ns, err := r1.GetNamespace(); err != nil {
+		t.Errorf("failed getting namespace: %v", err)
+	} else if ns != "ns" {
+		t.Errorf("unexpected namespace: %s", ns)
+	}
+
+	n2 := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("metadata: {name: r}"), n2); err != nil {
+		t.Fatal(err)
+	}
+	r2 := &RNode{N: n2}
+	if ns, err := r2.GetNamespace(); err != nil {
+		t.Errorf("failed getting namespace: %v", err)
+	} else if ns != "" {
+		t.Errorf("unexpected namespace: %s", ns)
+	}
+
+	n3 := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("kind: k"), n3); err != nil {
+		t.Fatal(err)
+	}
+	r3 := &RNode{N: n3}
+	if ns, err := r3.GetNamespace(); err != nil {
+		t.Errorf("failed getting namespace: %v", err)
+	} else if ns != "" {
+		t.Errorf("unexpected namespace: %s", ns)
+	}
+}
+
+func TestRNodeSetNamespace(t *testing.T) {
+	n1 := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("metadata: {namespace: ns1, name: r}"), n1); err != nil {
+		t.Fatal(err)
+	}
+	r1 := &RNode{N: n1}
+	if ns, err := r1.GetNamespace(); err != nil {
+		t.Errorf("failed getting namespace: %v", err)
+	} else if ns != "ns1" {
+		t.Errorf("unexpected namespace: %s", ns)
+	} else if err := r1.SetNamespace("ns2"); err != nil {
+		t.Errorf("failed setting namespace: %v", err)
+	} else if ns, err := r1.GetNamespace(); err != nil {
+		t.Errorf("failed getting namespace: %v", err)
+	} else if ns != "ns2" {
+		t.Errorf("unexpected namespace: %s", ns)
+	}
+}
+
+func TestRNodeGetName(t *testing.T) {
+	n1 := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("metadata: {namespace: ns, name: r}"), n1); err != nil {
+		t.Fatal(err)
+	}
+	r1 := &RNode{N: n1}
+	if ns, err := r1.GetName(); err != nil {
+		t.Errorf("failed getting name: %v", err)
+	} else if ns != "r" {
+		t.Errorf("unexpected name: %s", ns)
+	}
+
+	n2 := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("metadata: {namespace: ns}"), n2); err != nil {
+		t.Fatal(err)
+	}
+	r2 := &RNode{N: n2}
+	if nm, err := r2.GetName(); err != nil {
+		t.Errorf("failed getting name: %v", err)
+	} else if nm != "" {
+		t.Errorf("unexpected name: %s", nm)
+	}
+
+	n3 := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("kind: k"), n3); err != nil {
+		t.Fatal(err)
+	}
+	r3 := &RNode{N: n3}
+	if nm, err := r3.GetName(); err != nil {
+		t.Errorf("failed getting name: %v", err)
+	} else if nm != "" {
+		t.Errorf("unexpected name: %s", nm)
+	}
+}
+
+func TestRNodeSetName(t *testing.T) {
+	n1 := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("metadata: {namespace: ns1, name: r1}"), n1); err != nil {
+		t.Fatal(err)
+	}
+	r1 := &RNode{N: n1}
+	if nm, err := r1.GetName(); err != nil {
+		t.Errorf("failed getting namespace: %v", err)
+	} else if nm != "r1" {
+		t.Errorf("unexpected name: %s", nm)
+	} else if err := r1.SetName("r2"); err != nil {
+		t.Errorf("failed setting name: %v", err)
+	} else if nm, err := r1.GetName(); err != nil {
+		t.Errorf("failed getting name: %v", err)
+	} else if nm != "r2" {
+		t.Errorf("unexpected name: %s", nm)
+	}
+}
+
+func TestRNodeGetAnnotation(t *testing.T) {
+	n1 := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("metadata: {annotations: {foo: bar}}"), n1); err != nil {
+		t.Fatal(err)
+	}
+	r1 := &RNode{N: n1}
+	if v, err := r1.GetAnnotation("foo"); err != nil {
+		t.Errorf("failed getting annotation: %v", err)
+	} else if v != "bar" {
+		t.Errorf("unexpected value: %s", v)
+	}
+	if v, err := r1.GetAnnotation("foo1"); err != nil {
+		t.Errorf("failed getting annotation: %v", err)
+	} else if v != "" {
+		t.Errorf("unexpected value: %s", v)
+	}
+}
+
+func TestRNodeSetAnnotation(t *testing.T) {
+	n := &yaml.Node{}
+	if err := yaml.Unmarshal([]byte("kind: k"), n); err != nil {
+		t.Fatal(err)
+	}
+	r := &RNode{N: n}
+	if err := r.SetAnnotation("foo1", "bar1"); err != nil {
+		t.Errorf("failed setting annotation: %v", err)
+	} else if v, err := r.GetAnnotation("foo1"); err != nil {
+		t.Errorf("failed getting annotation: %v", err)
+	} else if v != "bar1" {
+		t.Errorf("unexpected value: %s", v)
+	} else if err := r.SetAnnotation("foo2", "bar2"); err != nil {
+		t.Errorf("failed setting annotation: %v", err)
+	} else if v, err := r.GetAnnotation("foo2"); err != nil {
+		t.Errorf("failed getting annotation: %v", err)
+	} else if v != "bar2" {
+		t.Errorf("unexpected value: %s", v)
+	}
+}
